33a19bd95fd0fc0a28688c42c34534321c05e920
Paul Tiplady <paul.tiplady@metaswitch.com>
2014-09-18 12:34:38 +0100

    Extend Graph to support BFS.

--- graphs/primitives.py
+++ graphs/primitives.py
@@ -1,3 +1,7 @@
+from collections import deque
+import logging
+
+_log = logging.getLogger(__name__)
 
 
 class GraphException(Exception):
@@ -5,40 +9,167 @@ class GraphException(Exception):
 
 
 class Node:
-    def __init__(self):
+    def __init__(self, label=None):
         self.edges = []
+        self.label = label
 
     def add_edge(self, edge):
         self.edges.append(edge)
 
+    def remove_edge(self, edge):
+        for i, this_edge in enumerate(self.edges):
+            if this_edge == edge:
+                del self.edges[i]
+                break
+        else:
+            raise GraphException('Edge %s not found on node %s.' % (edge, self))
+
+    def get_edges_by_label(self, label):
+        """Get first edge with the specified label."""
+        edges = []
+        for edge in self.edges:
+            if edge.label == label:
+                edges.append(edge)
+
+        if not edges:
+            # No hits
+            raise GraphException('No edge with label "%s"' % label)
+        else:
+            return edges
+
+    def __repr__(self):
+        return 'Node(%s)' % self.label
+
 
 class Edge:
-    def __init__(self, tail, head):
+    def __init__(self, tail, head, label=None):
         self.tail = tail
         self.head = head
+        self.label = label
+        self.tail.add_edge(self)
+        self.head.add_edge(self)
+
+    def get_other_node(self, node):
+        if self.tail == node:
+            return self.head
+        elif self.head == node:
+            return self.tail
+        else:
+            raise GraphException('Edge %s does not reference node %s' % (self, node))
+
+    def __repr__(self):
+        return 'Edge(%s, %s)' % (self.tail, self.head)
 
 
 class Graph:
     def __init__(self):
-        self.nodes = {}
-        self.edges = {}
+        self.nodes = []
+        self.edges = []
 
-    def add_node(self, label):
-        if label in self.nodes:
+    def add_node(self, node):
+        self.nodes.append(node)
+
+    def add_node_by_label(self, label):
+        if label in [node.label for node in self.nodes]:
             raise GraphException('Node %s already exists in graph.', label)
         else:
-            node = self.nodes[label] = Node()
+            node = Node(label=label)
+            self.add_node(node)
             return node
 
-    def add_edge(self, tail, head):
-        for node in [tail, head]:
-            if node not in self.nodes:
-                raise GraphException('Node %s does not exist in graph.', node)
+    def remove_node(self, node):
+        for i, this_node in enumerate(self.nodes):
+            if this_node == node:
+                del self.nodes[i]
+                # delete edges too?
+                break
+        else:
+            raise GraphException('Node %s not found.' % node)
 
-        # If we've not seen this pair of nodes before, initialize.
-        if (tail, head) not in self.edges:
-            self.edges[(tail, head)] = []
+    def add_edge(self, edge):
+        self.edges.append(edge)
 
-        edge = Edge(tail, head)
-        self.edges[(tail, head)].append(edge)
+    def add_edge_by_label(self, tail_label, head_label, edge_label=None):
+        for label in [tail_label, head_label]:
+            if label not in [node.label for node in self.nodes]:
+                raise GraphException('Node %s does not exist in graph.' % label)
+
+        tail = self.get_node_by_label(tail_label)
+        head = self.get_node_by_label(head_label)
+
+        edge = Edge(tail, head, label=edge_label)
+        self.add_edge(edge)
         return edge
+
+    def remove_edge(self, edge):
+        """Remove an edge from the Graph.
+
+        Also updates the edge-list of the nodes.
+        """
+        for i, this_edge in enumerate(self.edges):
+            if this_edge == edge:
+                del self.edges[i]
+                edge.head.remove_edge(edge)
+                edge.tail.remove_edge(edge)
+                break
+        else:
+            raise GraphException('Edge %s not found.' % edge)
+
+    def get_node_by_label(self, label):
+        """Get first node with the specified label."""
+        for node in self.nodes:
+            if node.label == label:
+                return node
+        else:
+            # No hits
+            raise GraphException('No node with label "%s"' % label)
+
+    def breadth_first_search(self, start, end):
+        """Perform a breadth-first search for a path from 'start' to 'end'."""
+        for node in self.nodes:
+            node.explored = None
+
+        # Put the starting node in the frontier list.
+        frontier = deque([start])
+        explored_list = []
+        # Nodes are marked explored once they are in the frontier
+        start.explored = True
+
+        found = False
+
+        # Loop until the frontier is empty (no more nodes to explore), or we find the target.
+        while frontier and not found:
+            _log.debug('Frontier: %s', frontier)
+            node = frontier.popleft()
+            _log.debug('Exploring node %s', node)
+            explored_list.append(node)
+            if node == end:
+                found = True
+            for edge in node.edges:
+                other_node = edge.get_other_node(node)
+                _log.debug('Examining other node %s (explored=%s)', other_node, other_node.explored)
+                if not other_node.explored:
+                    frontier.append(other_node)
+                    other_node.explored = True
+                    # Else other node was explored already, so ignore it.
+
+        return found, explored_list
+
+    def __eq__(self, other):
+        if other is None:
+            return False
+        elif len(self.nodes) != len(other.nodes):
+            return False
+        elif len(self.edges) != len(other.edges):
+            return False
+        else:
+            for node in self.nodes:
+                if node not in other.nodes:
+                    return False
+            for edge in self.edges:
+                if edge not in other.edges:
+                    return False
+        return True
+
+    def __repr__(self):
+        return 'Graph(nodes=%s, edges=%s)' % (self.nodes, self.edges)
--- graphs/test/test_primitives.py
+++ graphs/test/test_primitives.py
@@ -1,27 +1,81 @@
+import logging
 import unittest
-from graphs.primitives import Graph
+from graphs.primitives import Graph, GraphException
 
-__author__ = 'paul'
+_log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 
-class TestGraph(unittest.TestCase):
+class GraphTestCase(unittest.TestCase):
+    def _setup_basic_graph(self):
+        self.graph = Graph()
+        self.node_a = self.graph.add_node_by_label('a')
+        self.node_b = self.graph.add_node_by_label('b')
+        self.edge_a_b = self.graph.add_edge_by_label('a', 'b')
+
+    def _setup_triangle_graph(self):
+        self.graph = Graph()
+        self.node_a = self.graph.add_node_by_label('a')
+        self.node_b = self.graph.add_node_by_label('b')
+        self.node_c = self.graph.add_node_by_label('c')
+        self.edge_a_b = self.graph.add_edge_by_label('a', 'b')
+        self.edge_b_c = self.graph.add_edge_by_label('b', 'c')
+        self.edge_c_a = self.graph.add_edge_by_label('c', 'a')
+
+
+class TestGraph(GraphTestCase):
     def test_simple(self):
         """Test a simple 2-node graph with one edge."""
         graph = Graph()
-        a = graph.add_node('a')
-        b = graph.add_node('b')
-        a_b = graph.add_edge('a', 'b')
+        a = graph.add_node_by_label('a')
+        b = graph.add_node_by_label('b')
+        a_b = graph.add_edge_by_label('a', 'b')
 
-        self.assertEqual(graph.nodes, {'a': a, 'b': b})
-        self.assertEqual(graph.edges, {('a', 'b'): [a_b]})
+        self.assertTrue(a in graph.nodes)
+        self.assertTrue(b in graph.nodes)
+        self.assertTrue(a_b in graph.edges)
 
     def test_parallel(self):
         """Test a 2-node graph with parallel edges."""
         graph = Graph()
-        a = graph.add_node('a')
-        b = graph.add_node('b')
-        a_b_1 = graph.add_edge('a', 'b')
-        a_b_2 = graph.add_edge('a', 'b')
+        a = graph.add_node_by_label('a')
+        b = graph.add_node_by_label('b')
+        a_b_1 = graph.add_edge_by_label('a', 'b')
+        a_b_2 = graph.add_edge_by_label('a', 'b')
 
-        self.assertEqual(graph.nodes, {'a': a, 'b': b})
-        self.assertEqual(graph.edges, {('a', 'b'): [a_b_1, a_b_2]})
+        self.assertTrue(a in graph.nodes)
+        self.assertTrue(b in graph.nodes)
+        self.assertTrue(a_b_1 in graph.edges)
+        self.assertTrue(a_b_2 in graph.edges)
+
+    def test_add_edge_by_label(self):
+        """Test that an exception is raised when adding an edge with a non-existant label."""
+        self._setup_basic_graph()
+        self.assertRaises(GraphException, self.graph.add_edge_by_label, 'z', 'x')
+
+    def test_add_node_by_label(self):
+        """Test that an exception is raised when adding a node with a duplicate label."""
+        self._setup_basic_graph()
+        self.assertRaises(GraphException, self.graph.add_node_by_label, 'a')
+
+    def test_bfs(self):
+        """Test Breadth First Search in a graph."""
+        self._setup_triangle_graph()
+        self.node_d = self.graph.add_node_by_label('d')  # Unconnected node
+
+        found, explored = self.graph.breadth_first_search(self.node_a, self.node_c)
+
+        _log.info('Found? %s. Explored: %s', found, explored)
+        self.assertTrue(found)
+        self.assertEquals(explored, [self.node_a, self.node_b, self.node_c])
+
+    def test_bfs_fail(self):
+        """Test that BFS explores all nodes if it doesn't find the target."""
+        self._setup_triangle_graph()
+        self.node_d = self.graph.add_node_by_label('d')  # Unconnected node
+
+        found, explored = self.graph.breadth_first_search(self.node_a, self.node_d)
+
+        _log.info('Found? %s. Explored: %s', found, explored)
+        self.assertFalse(found)
+        self.assertEquals(explored, [self.node_a, self.node_b, self.node_c])
