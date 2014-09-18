62d4629295eb90d4e92b05b33deec7f6c57b8722
Paul Tiplady <paul.tiplady@metaswitch.com>
2014-09-18 12:36:40 +0100

    Implement bacon index script.

---
+++ graphs/kevin_bacon.py
@@ -0,0 +1,153 @@
+"""
+Bacon index calculator, using the IMDB movie graph.
+
+Usage:
+    kevin_bacon.py <actor_name>
+"""
+import logging
+from logging import basicConfig
+import re
+from docopt import docopt
+from graphs.primitives import Graph
+
+ACTOR_FILE = 'actors.list'
+ACTOR_RE = re.compile('([^\t]+)\t+([^\t]+)')
+TITLE_RE = re.compile('([\w .,"&!?\']+) (\(\d+\))')
+
+_log = logging.getLogger(__name__)
+basicConfig(level=logging.INFO)
+
+
+def _seek_to_actors(file):
+    """Given a file object, advance the read pointer to the first actual actor in the list."""
+    seeking = True
+    while seeking:
+        line = file.readline()
+        _log.debug('Read "%s"', line)
+        if line == "THE ACTORS LIST\n":
+            seeking = False
+
+    file.readline()  # Underline
+    file.readline()  # Blank line
+    headers = file.readline()
+    _log.debug('Read line "%s"', headers)
+    assert re.match('Name\t+Titles', headers)
+    file.readline()  # Underline
+    _log.debug('Finished seeking.')
+
+
+def _read_next_actor(file):
+    """Read and return the next actor from the file.
+
+    Assumes that actors are delimited by a blank line.
+    """
+    actor_lines = [file.readline()]
+    next_line = file.readline()
+
+    while next_line != '\n':
+        actor_lines.append(next_line)
+        next_line = file.readline()
+
+    return _format_actor_lines(actor_lines)
+
+
+def _format_actor_lines(lines):
+    """Parse the text of an Actor entry.
+
+    Expects the following input format:
+
+    "
+    Aackerlund, Thor        Ecstasy of Order: The Tetris Masters (2011)  [Himself]
+                            The NES Club (2015)  [Himself]
+                            "Culture x History x Attitude" (2012) {Dallas Comic-Con, Carolina Games Summit, Pinballz Arcade (#1.3)}  [Himself]
+
+    "
+
+    Where
+    RULES:
+    1       Movies and recurring TV roles only, no TV guest appearances
+    2       Please submit entries in the format outlined at the end of the list
+    3       Feel free to submit new actors
+
+    "xxxxx"        = a television series
+    "xxxxx" (mini) = a television mini-series
+    [xxxxx]        = character name
+    <xx>           = number to indicate billing position in credits
+    (TV)           = TV movie, or made for cable movie
+    (V)            = made for video movie (this category does NOT include TV
+                     episodes repackaged for video, guest appearances in
+                     variety/comedy specials released on video, or
+                     self-help/physical fitness videos)
+    """
+    actor_line = lines.pop(0)
+    _log.debug('Parsing actor line %s', actor_line)
+    match = ACTOR_RE.match(actor_line)
+    if match is None:
+        raise BaconException('Failed to match actor on line: %s' %actor_line)
+    actor, first_role = match.groups()
+
+    _log.debug('Got actor, first_role = %s, %s', actor, first_role)
+    title_strings = [first_role] + [title.strip() for title in lines]
+    titles = []
+
+    # Extract the titles from the list of title input strings
+    for title_string in title_strings:
+        match = TITLE_RE.match(title_string)
+        if match is None:
+            raise BaconException('Failed to match title on line: %s' % title_string)
+        titles.append(match.group(1))
+
+    return actor, titles
+
+
+def _create_actor_graph(actor_titles):
+    graph = Graph()
+    films_to_actors = {}
+
+    for actor, titles in actor_titles.items():
+        _log.debug('Processing actor "%s"', actor)
+        # First add a node for the actor
+        graph.add_node_by_label(actor)
+
+        # Next add edges to any actors with matching titles
+        for title in titles:
+            if title in films_to_actors:
+                # We've already got at least one actor from this title. Add edges.
+                for other_actor in films_to_actors[title]:
+                    graph.add_edge_by_label(actor, other_actor, edge_label=title)
+
+                # Also add this actor to the film's actor list, in case further matching actors are found.
+                films_to_actors[title].append(actor)
+            else:
+                # This film hasn't been seen before. Start a new actor list.
+                films_to_actors[title] = [actor]
+
+    return graph
+
+
+def main():
+    arguments = docopt(__doc__, version='0.1.0')
+    target_actor = arguments['<actor_name>']
+
+    with open(ACTOR_FILE, 'r', encoding='latin1') as actor_file:
+        actor_titles = {}
+        _seek_to_actors(actor_file)
+        actor, titles = _read_next_actor(actor_file)
+
+        while actor:
+            actor_titles[actor] = titles
+            actor, titles = _read_next_actor(actor_file)
+
+    if target_actor not in actor_titles:
+        print('Actor not found in list.')
+    else:
+        graph = _create_actor_graph(actor_titles)
+        graph.open_shortest_path('Kevin Bacon', target_actor)
+
+
+class BaconException(Exception):
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ graphs/test/test_kevin_bacon.py
@@ -0,0 +1,123 @@
+from unittest.case import TestCase
+from unittest.mock import patch, MagicMock
+from graphs.kevin_bacon import _seek_to_actors, _read_next_actor, BaconException, _create_actor_graph
+
+ACTOR_SAMPLE = """Aanaahad\t\tAkki, Vikki te Nikki (2014)  [Kuldeep]
+\t\tLahore (2010)  [Veerender Singh]  <3>
+
+Aanderaa, Torgny Gerhard\t\tCitizen X (2007)  [The drug-addict]
+\t\tKommandør Treholt & ninjatroppen (2010)  (as Torgny Aanderaa)  [Nyhetsstemmer]  <25>
+\t\tRegimet (2010)  [Inspector Hermansen]  <3>
+\t\tThe Secret World (2012) (VG)  [Misc characters]
+\t\t"Drømmen om Norge" (2005)  [Fin mann/Kelner/div. roller]
+\t\t"Helt perfekt" (2011) {Ikke lese, ikke jobbe (#1.6)}  [Vladimir]
+
+Aanderson, Chris\t\tDecoys 2: Alien Seduction (2007) (V)  [Guard]  <22>
+\t\tHigh Noon (2009) (TV)  [Detective 'Bull' Sykes]  <23>
+\t\t"Blackstone" (2011) {Bingo Night (#1.7)}  [Kevin Austin]  <26>
+\t\t"Fear Itself" (2008) {Family Man (#1.3)}  <13>
+\t\t"Heartland" (2007/II) {Man's Best Friend (#3.3)}  [Dwayne]  <14>
+\t\t"Mixed Blessings" (2007) {The Young Apprentice (#3.12)}  [Mr. Johnson]
+\t\t"Wild Roses" (2009) {Boom and Bust (#1.10)}  (uncredited)  [Junior Executive]
+
+"""
+
+LINKED_ACTORS_DICT = {
+    'Alice': [
+        'Film 1',
+        'Film 2',
+    ],
+    'Bob': [
+        'Film 1',
+        'Film 3',
+    ],
+    'Claire': [
+        'Film 1',
+        'Film 4',
+    ],
+    'Dan': [
+        'Film 3',
+    ],
+}
+
+
+class TestKevinBacon(TestCase):
+    def test_seek_to_actors(self):
+        """Test that _seek_to_actors correctly reads through the header."""
+        m_file = MagicMock()
+        m_file.readline.side_effect = iter([
+            'Blah! Blah! (blahblah)...\n',
+            'SOME OTHER TEXT\n',
+            '===============\n',
+            'THE ACTORS LIST\n',
+            '===============\n',
+            '\n',
+            'Name\t\tTitles\n',
+            '----\t\t------\n',
+            'DATA'
+        ])
+
+        _seek_to_actors(m_file)
+
+        self.assertEqual(len(m_file.mock_calls), 8)  # Count of lines excluding DATA
+        # DATA should be the next line read from the file
+        self.assertEqual(m_file.readline(), 'DATA')
+
+    def test_read_next_actor(self):
+        """Test that _read_next_actor correctly extracts actor data from text blob."""
+        m_file = MagicMock()
+        m_file.readline.side_effect = iter(ACTOR_SAMPLE.splitlines(keepends=True))
+
+        actor, titles = _read_next_actor(m_file)
+
+        self.assertEquals(actor, 'Aanaahad')
+        self.assertEquals(titles, ['Akki, Vikki te Nikki', 'Lahore'])
+
+        actor, titles = _read_next_actor(m_file)
+
+        self.assertEquals(actor, 'Aanderaa, Torgny Gerhard')
+        self.assertEquals(
+            titles,
+            [
+                'Citizen X',
+                'Kommandør Treholt & ninjatroppen',
+                'Regimet',
+                'The Secret World',
+                '"Drømmen om Norge"',
+                '"Helt perfekt"'
+            ]
+        )
+        
+    def test_read_next_actor_bad_actor(self):
+        """Test that _read_next_actor correctly complains on malformatted actor name."""
+        m_file = MagicMock()
+        m_file.readline.side_effect = iter(['ACTOR WITH_NO_TITLE\n', '\n'])
+
+        self.assertRaises(BaconException, _read_next_actor, m_file)
+
+    def test_read_next_actor_bad_title(self):
+        """Test that _read_next_actor correctly complains on malformatted titles."""
+        m_file = MagicMock()
+        m_file.readline.side_effect = iter(['ACTOR\t\tTITLE 1\n', 'TITLE WITH NO DATE', '\n'])
+
+        self.assertRaises(BaconException, _read_next_actor, m_file)
+
+    def test_create_actor_graph(self):
+        """Test that _create_actor_graph produces the expected Graph object."""
+        graph = _create_actor_graph(LINKED_ACTORS_DICT)
+
+        alice = graph.get_node_by_label('Alice')
+        bob = graph.get_node_by_label('Bob')
+        claire = graph.get_node_by_label('Claire')
+        dan = graph.get_node_by_label('Dan')
+
+        # Alice is in Film 1 with Bob and Claire
+        self.assertTrue('Film 1' in [edge.label for edge in alice.edges])
+        # Alice is not in Film 2 with either Bob or Claire
+        self.assertTrue('Film 2' not in [edge.label for edge in alice.edges])
+
+        # Check that we have the right neighbors for Alice
+        alice_partners = [edge.head if edge.tail.label == 'Alice' else edge.tail for edge in alice.edges]
+        self.assertTrue(bob in alice_partners)
+        self.assertTrue(claire in alice_partners)
+        self.assertTrue(dan not in alice_partners)
